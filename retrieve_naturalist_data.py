3a2e88e5a849ebccef1793dde87b001ce10d0971
Mc141 <mcwolmarans141@gmail.com>
2025-07-24 06:47:12 +0200

    format collected observations

--- retrieve_naturalist_data.py
+++ retrieve_naturalist_data.py
@@ -4,8 +4,8 @@ import datetime
 import math
 
 
-# Sometimes multiple api requests are needed, this collects all
-result_set = []
+# Sometimes multiple api requests are needed, this stores them
+pages = []
 
 
 # Api needs a date range
@@ -24,16 +24,16 @@ formatted_end_date = f"{end_date.year}-{end_date.month}-{end_date.day}"
 request_url = f"https://api.inaturalist.org/v1/observations?d1={start_date}&d2={end_date}&quality_grade=any&identifications=any&swlat=-34.43214105152007&swlng=18.252509856447368&nelat=-33.806848821450004&nelng=18.580726409181743&taxon_id=54053&verifiable=true&spam=false&fields=id,uuid,observed_on,time_observed_at,user_id,created_at,quality_grade,image_url,place_guess,latitude,longitude,positional_accuracy,private_place_guess,scientific_name,common_name"
 
 
-observations = requests.get(request_url)
+request_result = requests.get(request_url)
 
-observations_dict = observations.json()
+request_result_dict = request_result.json()
 
 
 # append firt page results to list
-result_set.append(observations_dict)
+pages.append(request_result_dict)
 
 
-observation_count = observations_dict['total_results']
+observation_count = request_result_dict['total_results']
 
 page_count = math.ceil(observation_count / 30)
 
@@ -43,29 +43,38 @@ if page_count > 1:
     # from page 2, as 1 has already been added
     for page in range(2, page_count + 1):
         request_url = f"https://api.inaturalist.org/v1/observations?page={page}&d1={start_date}&d2={end_date}&quality_grade=any&identifications=any&swlat=-34.43214105152007&swlng=18.252509856447368&nelat=-33.806848821450004&nelng=18.580726409181743&taxon_id=54053&verifiable=true&spam=false&fields=id,uuid,observed_on,time_observed_at,user_id,created_at,quality_grade,image_url,place_guess,latitude,longitude,positional_accuracy,private_place_guess,scientific_name,common_name"
-        observations = requests.get(request_url)
-        observations_dict = observations.json()
-        result_set.append(observations_dict)
+        request_result = requests.get(request_url)
+        request_result_dict = request_result.json()
+        pages.append(request_result_dict)
+
+
+observations = []
+
+
+for page in pages:
+    for observation in page['results']:
+        # If inaccurate, ignore observation
+        if (not isinstance(observation.get('positional_accuracy'), int)) or (observation.get('positional_accuracy') > 100): continue
+        
+        observation_dict = {
+            "id": observation['id'],
+            "uuid": observation.get('uuid'),
+            "time_observed_at": observation.get('time_observed_at'),
+            "created_at": observation.get('created_at'),
+            "latitude": observation.get('latitude'),
+            "longitude": observation.get('longitude'),
+            "positional_accuracy": observation.get('positional_accuracy'),
+            "place_guess": observation.get('place_guess'),
+            "scientific_name": observation.get('scientific_name'),
+            "common_name": observation.get('common_name'),
+            "quality_grade": observation.get('quality_grade'),
+            "image_url": observation.get('image_url'),
+            "user_id": observation.get('user_id')
+        }
+        
+        observations.append(observation_dict)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(observations_dict['results'][29]) # returns entry 29
-
-
+print(len(observations))
